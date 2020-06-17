85529307e26df37ad8bf313c1337205b362fb4f1
mantosh kumar <babu93078@gmail.com>
2020-06-18 00:05:35 +0530

    Initial Solution Setup

---
+++ MyGym/MyGym.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGym.WebUI.Startup))]
+namespace MyGym.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
