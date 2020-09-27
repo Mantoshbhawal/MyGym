2384cfb4a3d70f7bca9dab760de63de9a498f33d
jyoti bharti <babu93078@gmail.com>
2020-09-28 04:44:58 +0530

    Product Grouping Adding

--- MyGym/MyGym.WebUI/Controllers/ProductManagerController.cs
+++ MyGym/MyGym.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyGym.Core.Models;
+using MyGym.Core.ViewModels;
 using MyGym.DataAccess.InMemory;
 
 namespace MyGym.WebUI.Controllers
@@ -11,10 +12,12 @@ namespace MyGym.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
 
         }
         // GET: ProductManager
@@ -27,8 +30,10 @@ namespace MyGym.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product  = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -56,7 +61,10 @@ namespace MyGym.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
         [HttpPost]
